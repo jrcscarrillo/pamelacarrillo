b4648be9b5c360f9e3ec9b397450560563c12ec5
Juan R Carrillo <jcarrillo1@madisoncollege.edu>
2018-08-30 11:55:48 -0500

    La pagina de la Pamelita

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "From your website'"; //letter subject
+	$to = 'pamelacarrillo@pamelacarrillo.info'; // Enter Your E-mail
+	$from = 'jrcscarrillo@gmail.com'; // Admin e-mail
+	$fromName = 'Carrillos Team'; // Your company name
+	$charset = 'UTF-8';
+?>
